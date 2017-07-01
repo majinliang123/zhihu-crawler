33e5ad0710b4e519023a09460f3295bc1e14d762
Madison <18233698150@163.com>
2017-07-01 10:17:39 +0800

    use many process

--- cluster/master.js
+++ cluster/master.js
@@ -3,8 +3,7 @@
 const child_process = require('child_process');
 const numCPUs = require('os').cpus().length;
 
-const child = require('./child.js');
 
 for (let i = 0; i < numCPUs; i++) {
-    child_process.fork(child);
+    child_process.fork(__dirname + "/child.js");
 }
